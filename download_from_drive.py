8703a37421b9860c9fe844f09272ad05a5fc976b
lungsang-bod <lungsang@esukhia.org>
2022-10-10 12:17:38 +0530

    server path update

--- download_from_drive.py
+++ download_from_drive.py
@@ -30,7 +30,7 @@ CLIENT_SECRET_FILE = 'client_secrets.json'
 APPLICATION_NAME = 'levelpack-UI'
 
 # Declare full path to folder and folder name
-FULL_PATH = r'/home/lungsang/Desktop/levelpack-UI/content'
+FULL_PATH = r'/home/ubuntu/levelpack-UI/content'
 DIR_NAME = 'content'
 # Or simply
 # DIR_NAME = FULL_PATH.split('/')[-1]
